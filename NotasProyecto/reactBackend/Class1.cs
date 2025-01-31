ff4b985307971b3be0b56a7521ac2ad62eb56baf
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2025-01-31 10:15:58 -0600

    Guía II - Uso de Entity Framework

---
+++ NotasProyecto/reactBackend/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace reactBackend
+{
+    public class Class1
+    {
+        //Data Source =.\SQLEXPRESS;
+        //Initial Catalog = registroAlumnos; 
+        //User ID = Alondra; 
+        //Password = 12345
+        //Encrypt=False;
+        //Microsoft.EntityFrameworkCore
+
+
+        //Scaffold-DbContext "Data Source=.\SQLEXPRESS; Initial Catalog=registroAlumnos;User ID=Alondra;Password = 12345; Encrypt=False" Microsoft.EntityFrameworkCore.SqlServer -ContextDir "Context" -OutputDir "Models"
+    }
+}
