9f2119886142c9fded596ffee14f52617f3ddf01
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2025-02-01 22:36:36 -0600

    Guía VI - Introducción ASP.NET

---
+++ NotasProyecto/WebApi/Controllers/PruebaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+        // Inicia la URL
+        [HttpGet("prueba")]
+
+        // Metodo pulbico que se ejecutara si la url es llamada
+        public string Get()
+        {
+            return "Hola Mundo";
+        }
+    }
+}
